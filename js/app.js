864641aaa408fe251dec7d01a1c82dcc24d4e4b4
Nazarii14 <rosaka1429@gmail.com>
2024-01-27 10:14:55 +0200

    added animations for skills section

--- js/app.js
+++ js/app.js
@@ -1,40 +1,19 @@
-const sectionHeadingObserver = new IntersectionObserver((entries) => {
+const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-          entry.target.classList.add('show');
-      }
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
   });
 });
 
-const sectionHeadings = document.querySelectorAll('.section-heading');
-sectionHeadings.forEach((sectionHeading) => {
-  sectionHeadingObserver.observe(sectionHeading);
-});
+const descriptionHeaders = document.querySelectorAll('.description-header');
+descriptionHeaders.forEach((header) => observer.observe(header));
 
-// const aboutHobbiesTextsObserver = new IntersectionObserver((entries) => {
-//   entries.forEach((entry) => {
-//       if (entry.isIntersecting) {
-//           entry.target.classList.add('show-about-hobbies-text');
-//       }
-//   });
-// });
+const paragraphs = document.querySelectorAll('.about-description p');
+paragraphs.forEach((paragraph) => observer.observe(paragraph));
 
-// const skillsObserver = new IntersectionObserver((entries) => {
-//     entries.forEach((entry) => {
-//         if (entry.isIntersecting) {
-//             entry.target.classList.add('show');
-//         }
-//     });
-// });
 
-// const aboutHobbiesSvg = document.querySelectorAll("#about-text path");
-// aboutHobbiesSvg.forEach((element) => aboutHobbiesTextsObserver.observe(element));
 
-// const hiddenHardSkillsElements = document.querySelectorAll('.hard-skills-list-hidden');
-// hiddenHardSkillsElements.forEach((element) => skillsObserver.observe(element));
-
-const aboutText = document.querySelectorAll('#about-text path');
-const hobbiesText = document.querySelectorAll('#hobbies-text path');
 
 
 // side menu
@@ -47,25 +26,6 @@ function openmenu() {
 function closemenu() {
   sidemenu.style.right = "-200px";
 }
-
-//contact form
-const scriptURL = "https://script.google.com/macros/s/AKfycbyOeuPn9ul10ylJzCEGsm0A8YDYsi_qse82_banpcXOvNBaG7Pu6rhJ09MT4UbyklGN/exec";
-const form = document.getElementById('contact-form');
-const msg = document.getElementById("msg");
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  fetch(scriptURL, { method: "POST", body: new FormData(form) })
-    .then((response) => {
-      alert("message!")
-      msg.textContent = "Message sent successfully!";
-      setTimeout(function () {
-        msg.textContent = "";
-      }, 5000);
-      form.reset();
-    })
-    .catch((error) => console.error("Error!", error.message));
-});
 
 
 const container = document.querySelector('.education-list');
---
+++ js/contactForm.js
@@ -0,0 +1,13 @@
+const scriptURL = "https://script.google.com/macros/s/AKfycbyOeuPn9ul10ylJzCEGsm0A8YDYsi_qse82_banpcXOvNBaG7Pu6rhJ09MT4UbyklGN/exec";
+const form = document.getElementById('contact-form');
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      alert("Message sent successfully!")
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
